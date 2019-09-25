40a9b6a6bfa5284f5b886741f1157956df5e127a
Andrey Draganchuk <adraganchuk@simtechdev.com>
2019-09-25 14:00:42 +0400

    Rework request class

--- src/Framework/Http/Request.php
+++ src/Framework/Http/Request.php
@@ -18,20 +18,22 @@ class Request
         return $this->queryParams;
     }
 
-    public function withQueryParams(array $query)
-    {
-        $this->queryParams = $query;
-        return $this;
-    }
-
-    public function withParsedBody($data)
-    {
-        $this->queryParams = $data;
-        return $this;
-    }
-
     public function getParsedBody()
     {
         return $this->parsedBody;
+    }
+
+    public function withQueryParams(array $query): self
+    {
+        $new = clone $this;
+        $new->queryParams = $query;
+        return $new;
+    }
+
+    public function withParsedBody($data): self
+    {
+        $new = clone $this;
+        $new->parsedBody = $data;
+        return $new;
     }
 }
