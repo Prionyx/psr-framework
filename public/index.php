315887839661e5ce3079deec67be467e8cd6e367
Andrey Draganchuk <adraganchuk@simtechdev.com>
2019-09-26 13:59:19 +0400

    Add Response class

--- public/index.php
+++ public/index.php
@@ -8,5 +8,12 @@ require 'vendor/autoload.php';
 $request = RequestFactory::fromGlobals();
 
 $name = $request->getQueryParams()['name'] ?? 'Guest';
-header('X-Developer: Test');
-echo 'Hello, ' . $name . '!';
+
+$response = (new Response('Hello, ' . $name . '!'))
+    ->withHeader('X-Developer', 'Test');
+
+header('HTTP/1.0' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+foreach ($response->getHeaders() as $$name => $value) {
+    header($name . ': . $value');
+}
+echo $response->getBody();
