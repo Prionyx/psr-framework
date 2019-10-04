209511bb1d9fae32d92ab06a667f4866f6754f98
Andrey Draganchuk <spirifex@gmail.com>
2019-10-04 13:42:08 +0400

    Add routing

--- public/index.php
+++ public/index.php
@@ -1,18 +1,47 @@
 <?php
 
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
+### Initialization
+
 $request = ServerRequestFactory::fromGlobals();
 
-$name = $request->getQueryParams()['name'] ?? 'Guest';
+### Action
 
-$response = (new HtmlResponse('Hello, ' . $name . '!'))
-    ->withHeader('X-Developer', 'Test');
+$path = $request->getUri()->getPath();
+
+if ($path === '/') {
+    $name = $request->getQueryParams()['name'] ?? 'Guest';
+    $response = (new HtmlResponse('Hello, ' . $name . '!'));
+} elseif ($path === '/about') {
+    $response = new HtmlResponse('I am a simple site');
+} elseif ($path === '/blog') {
+    $response = new JsonResponse([
+        ['id' => 2, 'title' => 'The Second Post'],
+        ['id' => 1, 'title' => 'The First Post'],
+    ]);
+} elseif (preg_match('#^/blog/(?P<id>\d+)$#i', $path, $matches)) {
+    $id = $matches['id'];
+    if ($id > 2) {
+        $response = new JsonResponse(['error' => 'Undefined page'], 404);
+    } else {
+        $response = new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+    }
+} else {
+    $response = new JsonResponse(['error' => 'Undefined page'], 404);
+}
+
+### Postprocessing
+
+$response = $response->withHeader('X-Developer', 'Test');
+
+### Sending
 
 $emitter = new SapiEmitter();
 $emitter->emit($response);
