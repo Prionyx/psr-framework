93564fe29861b6ea00fb11e044f158eae7f0a272
Andrey Draganchuk <spirifex@gmail.com>
2019-10-12 22:55:43 +0400

    Add auth decorator

--- src/App/Http/Action/CabinetAction.php
+++ src/App/Http/Action/CabinetAction.php
@@ -3,29 +3,14 @@
 namespace App\Http\Action;
 
 use Psr\Http\Message\ServerRequestInterface;
-use Zend\Diactoros\Response\EmptyResponse;
 use Zend\Diactoros\Response\HtmlResponse;
 
 class CabinetAction
 {
-    public function __construct(array $users)
-    {
-        $this->users = $users;
-    }
-
     public function __invoke(ServerRequestInterface $request)
     {
-        $username = $request->getServerParams()['PHP_AUTH_USER'] ?? null;
-        $password = $request->getServerParams()['PHP_AUTH_PW'] ?? null;
+        $username = $request->getAttribute('username');
 
-        if (!empty($username) && !empty($password)) {
-            foreach ($this->users as $name => $pass) {
-                if ($username === 'admin' && $password === 'password') {
-                    return new HtmlResponse('I am logged in as ' . $username);
-                }
-            }
-        }
-
-        return new EmptyResponse(401, ['WWW-Authenticate' => 'Basic realm=Restricted area']);
+        return new HtmlResponse('I am logged in as ' . $username);
     }
 }
