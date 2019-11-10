32640e4533c0c43c01d1ddabdfa5394b712d9cca
Andrey Draganchuk <spirifex@gmail.com>
2019-11-10 14:00:12 +0400

    Add caching to container

--- src/Framework/Container/Container.php
+++ src/Framework/Container/Container.php
@@ -5,9 +5,14 @@ namespace Framework\Container;
 class Container
 {
     private $definitions = [];
+    private $results = [];
 
     public function get($id)
     {
+        if (array_key_exists($id, $this->results)) {
+            return $this->results[$id];
+        }
+
         if (!array_key_exists($id, $this->definitions)) {
             throw new ServiceNotFoundException('Uncnown sevice "' . $id . '"');
         }
@@ -15,16 +20,20 @@ class Container
         $definitions = $this->definitions[$id];
 
         if ($definitions instanceof \Closure) {
-            $result = $definitions();
+            $this->results[$id] = $definitions();
         } else {
-            $result = $definitions;
+            $this->results[$id] = $definitions;
         }
 
-        return $result;
+        return $this->results[$id];
     }
 
     public function set($id, $value): void
     {
+        if (array_key_exists($id, $this->results)) {
+            unset($this->results[$id]);
+        }
+
         $this->definitions[$id] = $value;
     }
 }
