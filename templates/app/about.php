5d86b3d09e539b9ad66cbdf370798dc41b886320
Andrey Draganchuk <adraganchuk@simtechdev.com>
2019-11-28 18:00:23 +0400

    Add block callbacks

--- templates/app/about.php
+++ templates/app/about.php
@@ -12,10 +12,12 @@
     <meta name="description" content="About Page description" />
 <?php $this->endBlock(); ?>
 
-<ul class="breadcrumb">
-    <li><a href="/">Home</a></li>
-    <li class="active">About</li>
-</ul>
+<?php $this->beginBlock('breadcrumbs') ?>
+    <ul class="breadcrumb">
+        <li><a href="/">Home</a></li>
+        <li class="active">About</li>
+    </ul>
+<?php $this->endBlock() ?>
 
 <?php $this->beginBlock('main') ?>
     <h1>About the site</h1>
--- templates/app/cabinet.php
+++ templates/app/cabinet.php
@@ -8,21 +8,22 @@
 
 <?php $this->beginBlock('title') ?>Cabinet<?php $this->endBlock() ?>
 
-<?php $this->beginBlock('sidebar') ?>
-    <div class="panel panel-default">
-        <div class="panel-heading">Cabinet</div>
-        <div class="panel-body">Cabinet navigation</div>
-    </div>
+<?php $this->beginBlock('breadcrumbs') ?>
+    <ul class="breadcrumb">
+        <li><a href="/">Home</a></li>
+        <li class="active">Cabinet</li>
+    </ul>
 <?php $this->endBlock(); ?>
 
 <?php $this->beginBlock('main') ?>
     <h1>
         Cabinet of <?= htmlspecialchars($name, ENT_QUOTES | ENT_SUBSTITUTE) ?>
     </h1>
+<?php $this->endBlock(); ?>
 
-<ul class="breadcrumb">
-    <li><a href="/">Home</a></li>
-    <li class="active">Cabinet</li>
-</ul>
-
+<?php $this->beginBlock('sidebar') ?>
+    <div class="panel panel-default">
+        <div class="panel-heading">Cabinet</div>
+        <div class="panel-body">Cabinet navigation</div>
+    </div>
 <?php $this->endBlock(); ?>
